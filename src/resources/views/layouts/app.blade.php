7daacd82eb61c7b33ad78cd5a5eead3e80a48d62
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-10-24 10:50:46 +0900

    app-view作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-container">
+            <div id="drawer_toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <h1 class="header__title">Rese</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="main-container">
+            @yield('content')
+        </div>
+    </main>
+</body>
+</html>
