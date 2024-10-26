7e50d4bb295e52fcf13a12d628c26c0626b4badc
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-10-26 09:43:09 +0900

    header menu作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,14 +12,51 @@
 <body>
     <header class="header">
         <div class="header-container">
-            <div id="drawer_toggle">
+            <div id="drawer-toggle">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
             <h1 class="header__title">Rese</h1>
+
+            <nav class="header-nav__menu" id="header-nav">
+                <ul class="header-nav__list">
+                    <li class="header-nav__item">
+                        <a class="header-nav__link" href="/">
+                            Home
+                        </a>
+                    </li>
+                    @if (Auth::check())
+                        <li class="header-nav__item">
+                            <form action="" method="">
+                                <button class="header-nav__link" type="submit">
+                                    Logout
+                                </button>
+                            </form>
+                            <!-- <a class="header-nav__link" href="">
+                                Logout
+                            </a> -->
+                        </li><li class="header-nav__item">
+                            <a class="header-nav__link" href="/mypage">
+                                Mypage
+                            </a>
+                        </li>
+                    @else
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/register">
+                                Registration
+                            </a>
+                        </li><li class="header-nav__item">
+                            <a class="header-nav__link" href="/login">
+                                Login
+                            </a>
+                        </li>
+                    @endif
+                </ul>
+            </nav>
         </div>
         @yield('search')
+        <script src="{{ asset('js/header-nav.js') }}"></script>
     </header>
 
     <main class="main">
