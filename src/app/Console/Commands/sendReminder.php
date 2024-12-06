839cdc4c3a842e282d5c0bee48ef756ce0b877ca
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-12-06 11:19:37 +0900

    リマインダー送信機能作成

---
+++ src/app/Console/Commands/sendReminder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Models\Reservation;
+use App\Notifications\ReservationReminder;
+
+class sendReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:sendReminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'to send reminder-mail';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = Carbon::today()->format('Y-m-d');
+        $reservations = Reservation::with('user')->where('date', $today)->get();
+        foreach($reservations as $reservation) {
+            ($reservation->user)->notify(new ReservationReminder ($reservation));
+        }
+    }
+}
